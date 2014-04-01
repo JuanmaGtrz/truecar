e1416bcd3de8cbe2f7885fca58bcc789de14fbcf
JuanmaGtrz <juanmgutad@gmail.com>
2014-04-01 19:15:55 +0200

    version2.0

---
+++ truecar.js
@@ -0,0 +1,11 @@
+/**
+* Created with JQUERYMOBILE.
+* User: Juanma
+* Date: 2014-03-26
+* Time: 05:32 PM
+* To change this template use Tools | Templates.
+*/
+
+window.addEventListener('load', function() {
+                        FastClick.attach(document.body);
+                        }, false);
